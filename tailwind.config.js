94928adcaf2a2287874e60627cbabb54b987f077
veshall <vishalpaliwal51@gmail.com>
2023-06-20 11:24:11 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        lexend: ["Lexend Deca", " sans-serif"],
+      },
+      colors: {
+        input: "#E5E5E5",
+        background: "#224957",
+        button: "#20DF7F",
+      },
+      borderRadius: {
+        morelg: "10px",
+      },
+      boxShadow: {
+        button: "0px 4px 4px rgba(0, 0, 0, 0.3)",
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/forms")],
+};
